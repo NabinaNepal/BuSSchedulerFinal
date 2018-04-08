42bf78493123aa43833bce66ca36e21dd30f1a00
Sunil Lama <rumba.alex47@gmail.com>
2018-04-08 19:52:29 +0545

    static lat long

--- src/pages/near-bystops/near-bystops.ts
+++ src/pages/near-bystops/near-bystops.ts
@@ -38,33 +38,49 @@ export class NearBystopsPage {
   }
 
   initMap() {
-    this.platform.ready().then(() => {
-      console.log("Device is ready! View did enter!");
-      let options = {
-        enableHighAccuracy: true,
-        timeout: 10000,
-        maximumAge: 0
-      }
-      this.geolocation.getCurrentPosition(options).then((resp) => {
-        var pyrmont = { lat: resp.coords.latitude, lng: resp.coords.longitude };
+    //var pyrmont = {lat: -33.867, lng: 151.195};
+    var pyrmont = { lat: 38.758034, lng: -93.739136 };
 
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: pyrmont,
-          zoom: 15
-        });
-
-        infowindow = new google.maps.InfoWindow();
-        var service = new google.maps.places.PlacesService(map);
-        service.nearbySearch({
-          location: pyrmont,
-          radius: 500,
-          type: ['store']
-        }, this.callback.bind(this));
-        // alert("My position: " + resp.coords.latitude + ", " + resp.coords.longitude);
-      }).catch((error) => {
-        alert("Error getting location Code: " + error.code + ", Message: " + error.message);
-      });
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: pyrmont,
+      zoom: 15
     });
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+      location: pyrmont,
+      radius: 500,
+      type: ['busstops']
+    }, this.callback.bind(this));
+
+    // this.platform.ready().then(() => {
+    //   console.log("Device is ready! View did enter!");
+    //   let options = {
+    //     enableHighAccuracy: true,
+    //     timeout: 10000,
+    //     maximumAge: 0
+    //   }
+    //   this.geolocation.getCurrentPosition(options).then((resp) => {
+    //     var pyrmont = { lat: resp.coords.latitude, lng: resp.coords.longitude };
+
+    //     map = new google.maps.Map(document.getElementById('map'), {
+    //       center: pyrmont,
+    //       zoom: 15
+    //     });
+
+    //     infowindow = new google.maps.InfoWindow();
+    //     var service = new google.maps.places.PlacesService(map);
+    //     service.nearbySearch({
+    //       location: pyrmont,
+    //       radius: 500,
+    //       type: ['store']
+    //     }, this.callback.bind(this));
+    //     // alert("My position: " + resp.coords.latitude + ", " + resp.coords.longitude);
+    //   }).catch((error) => {
+    //     alert("Error getting location Code: " + error.code + ", Message: " + error.message);
+    //   });
+    // });
 
   }
 
