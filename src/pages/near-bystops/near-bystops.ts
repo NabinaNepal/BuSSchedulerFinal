c2b234746223f4f269d4ab4845111f2cfb8869c0
Sunil Lama <rumba.alex47@gmail.com>
2018-04-08 13:04:24 +0545

    changes for the nearby shops

--- src/pages/near-bystops/near-bystops.ts
+++ src/pages/near-bystops/near-bystops.ts
@@ -38,20 +38,34 @@ export class NearBystopsPage {
   }
 
   initMap() {
-    var pyrmont = { lat: -33.867, lng: 151.195 };
+    this.platform.ready().then(() => {
+      console.log("Device is ready! View did enter!");
+      let options = {
+        enableHighAccuracy: true,
+        timeout: 10000,
+        maximumAge: 0
+      }
+      this.geolocation.getCurrentPosition(options).then((resp) => {
+        var pyrmont = { lat: resp.coords.latitude, lng: resp.coords.longitude };
 
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: pyrmont,
-      zoom: 15
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: pyrmont,
+          zoom: 15
+        });
+
+        infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
+          location: pyrmont,
+          radius: 500,
+          type: ['store']
+        }, this.callback.bind(this));
+        // alert("My position: " + resp.coords.latitude + ", " + resp.coords.longitude);
+      }).catch((error) => {
+        alert("Error getting location Code: " + error.code + ", Message: " + error.message);
+      });
     });
 
-    infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-      location: pyrmont,
-      radius: 500,
-      type: ['store']
-    }, this.callback.bind(this));
   }
 
 
